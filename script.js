e0b60e2d63c40034e4fd03c9cc48529ef5f94b40
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-04-19 16:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-	
+	for (let i = 0; i < arr.length; i++) {
+		for (let t = 1; t < arr.length; t++) {
 			if (arr[i] + arr[t] == n) {
 				console.log([[arr[i], arr[t]]]);
 				
